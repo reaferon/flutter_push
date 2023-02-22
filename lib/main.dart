0a484a76e00175f4acaf40100b2517570d7e1e17
Andy Pilyugin <reaferon@ya.ru>
2023-02-22 11:18:09 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String millisecondsText = "";
+  int buttonColor = 0xFF40CA88;
+  GameState gameState = GameState.readyToStart;
+  Timer? waitingTimer;
+  Timer? stoppableTimer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF282E3D),
+      body: Stack(
+        children: [
+          Align(
+              alignment: const Alignment(0, -0.9),
+              child: Text(
+                "Test your\nreaction speed",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white),
+              )),
+          Align(
+              alignment: Alignment.center,
+              child: ColoredBox(
+                color: Color(0xFF6D6D6D),
+                child: SizedBox(
+                  height: 160,
+                  width: 300,
+                  child: Center(
+                    child: Text(
+                      millisecondsText,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              )),
+          Align(
+              alignment: const Alignment(0, 0.9),
+              child: GestureDetector(
+                onTap: () => setState(() {
+                  switch (gameState) {
+                    case GameState.readyToStart:
+                      gameState = GameState.waiting;
+                      millisecondsText = "";
+                      _startWaitingTimer();
+                      break;
+                    case GameState.waiting:
+                      break;
+                    case GameState.canBeStopped:
+                      gameState = GameState.readyToStart;
+                      stoppableTimer?.cancel();
+                      break;
+                  }
+                }),
+                child: ColoredBox(
+                  color: Color(buttonColor),
+                  child: SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: Center(
+                      child: Text(
+                        _getButtonText(),
+                        style: TextStyle(
+                            fontSize: 38,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+
+  String _getButtonText() {
+    switch (gameState) {
+      case GameState.readyToStart:
+        buttonColor = 0xFF40CA88;
+        return "START";
+      case GameState.waiting:
+        buttonColor = 0xFFE0982D;
+        return "WAIT";
+      case GameState.canBeStopped:
+        buttonColor = 0xFFE02D47;
+        return "STOP";
+    }
+  }
+
+  void _startWaitingTimer() {
+    final int randomMilliseconds = Random().nextInt(4000) + 1000;
+    waitingTimer = Timer(Duration(milliseconds: randomMilliseconds), () {
+      setState(() {
+        gameState = GameState.canBeStopped;
+      });
+      _startStoppableTimer();
+    });
+  }
+
+  void _startStoppableTimer() {
+    stoppableTimer = Timer.periodic(Duration(milliseconds: 16), (timer) {
+      setState(() {
+        millisecondsText = "${timer.tick * 16} ms";
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    waitingTimer?.cancel();
+    stoppableTimer?.cancel();
+    super.dispose();
+  }
+}
+
+enum GameState { readyToStart, waiting, canBeStopped }
